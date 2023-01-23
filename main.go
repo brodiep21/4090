a87e1c3108a22264c9eaf71e413d1ed964ae0b08
Brodie Peif <brodiep21@hotmail.com>
2023-01-23 16:36:15 -0500

    first commit scraper

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(colly.AllowedDomains("quotes.toscrape.com"))
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Response Code", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Error", err)
+	})
+	c.Visit("https://quotes.toscrape.com")
+}
