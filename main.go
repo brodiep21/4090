f4f363db4d734708f29fadcd1d6fd65512c5d31d
Brodie Peif <brodiep21@hotmail.com>
2023-01-24 16:54:34 -0500

    working on DOM traversal

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	c := colly.NewCollector(colly.AllowedDomains("quotes.toscrape.com"))
+	c := colly.NewCollector(colly.AllowedDomains("www.newegg.com"))
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
@@ -21,5 +21,11 @@ func main() {
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Error", err)
 	})
-	c.Visit("https://quotes.toscrape.com")
+	c.Visit("https://www.newegg.com")
+
+	c.OnHTML(".section-title", func(h *colly.HTMLElement) {
+		words := h.ChildText("h2.section-title-text.font-xxxl.text-brandblue")
+		// fmt.Println("blahblah")
+		fmt.Println(words)
+	})
 }
