fa5ec960736768dc84f387bbe9546579c4e35c7b
Brodie Peif <brodiep21@hotmail.com>
2023-02-05 19:53:41 -0500

    working email with scraper

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "fmt"
+
+	"github.com/brodiep21/4090/search"
+	// "github.com/gocolly/colly"
+	// "net/smtp"
+)
+
+func main() {
+	search.SearchNewEgg()
+	// internal.Mailinfo()
+}
